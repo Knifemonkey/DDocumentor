5d7bafce9e712c713694032fcf008129dbf7af96
alexnegrila <alexnegrila@gmail.com>
2013-04-15 20:41:20 +0100

    I have added a class Prettify that handles the conversion of jova source code to styled html. I ajusted the outputHTML.html file to handle the new result

--- src/main/java/org/ddocumentor/Convert.java
+++ src/main/java/org/ddocumentor/Convert.java
@@ -1,13 +1,15 @@
 package org.ddocumentor;
 
+import java.io.IOException;
+
 public class Convert {
 	
-	public String convert(String s){
-		
+	public String convert(String s) throws IOException{
 		String startDelimiter = "#DocStart";
 		int codeDocStart = s.indexOf(startDelimiter)+startDelimiter.length();
 		int codeDocEnd = s.indexOf("#DocEnd")-2;
 		CharSequence codeExample = s.subSequence(codeDocStart, codeDocEnd);
-		return "<html><body>" + codeExample + "</body></html>";
+		return "<html><body>" + new Prettify().prettify(codeExample.toString()) + "</body></html>";
 	}
+
 }
